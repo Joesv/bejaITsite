292189ac2fc19c3f674d8ef14d159269707906b9
Joesv <hesseljoes@gmail.com>
2021-01-29 23:04:38 +0100

    rondere lijnen bij t grafiek

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include_once("includes/header.php"); ?>
+
+    <div id="mapContainer"><canvas id="map"></canvas></div>
+
+    <div id="map" style="width: 1920px; height: 1080px;"></div>
+   <!-- <script src="js/mappa.js" type="text/javascript"></script> -->
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+            integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+            crossorigin=""></script>
+    <script src="js/main.js" type="text/javascript"></script>
+<?php
+include_once("includes/footer.php");
